03e8453ceb630ee32dd5b0bc4a4144f0aa554612
kumitatepazuru <riku.ueda@outlook.jp>
2021-05-07 07:50:53 +0900

    バグ修正

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ import logging
 from discord.ext import commands
 
 
-async def p(ctx, *pr, sep=' ', end='\n', file=None, flush: bool = False):
-    PRINT_TXT = sep.join(*pr) + end
-    await ctx.send("```python\n" + PRINT_TXT + "\n```")
-
-
 class compile(commands.Cog):
-
     def __init__(self, bot):
         self.bot = bot
         self.logger = logging.getLogger(__name__)
 
     @commands.command(aliases=['cep'])
     async def compile_python(self, ctx, args):
-        exec(args, {'__builtins__': None, "print": lambda *ar, **kwargs: await p(ctx, ar, kwargs)}, {})
+        PRINT_TXT = ""
+
+        def p(*pr, sep=' ', end='\n', file=None, flush: bool = False):
+            global PRINT_TXT
+            PRINT_TXT += sep.join(*pr) + end
+
+        exec(args, {'__builtins__': None, "print": p}, {})
+        ctx.send("```python\n" + PRINT_TXT + "\n```")
 
 
 def setup(bot):
