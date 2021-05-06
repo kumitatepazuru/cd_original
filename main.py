3cbd28ed1915b3403943d3e19bb9ee21e49fe57f
kumitatepazuru <riku.ueda@outlook.jp>
2021-05-07 07:36:23 +0900

    バグ修正

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ class compile(commands.Cog):
         self.logger = logging.getLogger(__name__)
 
     @commands.command(aliases=['cep'])
-    async def compile_python(self, ctx, *args):
+    async def compile_python(self, ctx, args):
         def p(*print, sep=' ', end='\n', file=None, flush: bool = False):
             PRINT_TXT = sep.join(*print) + end
             ctx.send("```python\n" + PRINT_TXT + "\n```")
-        exec('import os;os.system("echo danger!")', {'__builtins__': None}, {})
+        exec(args, {'__builtins__': None, "print":p}, {})
 
 
 def setup(bot):
